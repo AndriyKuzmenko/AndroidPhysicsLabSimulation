a1a8a4fdb1ce02cec4ccc702008a757bc30a6315
Andriy <ak8022@bs.amalnet.k12.il>
2022-01-21 20:18:30 +0200

    display results

--- app/src/main/java/com/example/androidphysicslab/SpringResults.java
+++ app/src/main/java/com/example/androidphysicslab/SpringResults.java
@@ -1,11 +1,25 @@
 package com.example.androidphysicslab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class SpringResults extends AppCompatActivity
 {
+    TextView mgkView;
+    ListView resultsSpringLV;
+    Button plotsSpringButton,menuSpringButton;
+    double[] xList,vList,aList;
+    double m,g,k;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -13,5 +27,94 @@ public class SpringResults extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spring_results);
+
+        mgkView=(TextView)findViewById(R.id.mgkView);
+        resultsSpringLV=(ListView) findViewById(R.id.resultsSpringLV);
+        plotsSpringButton=(Button)findViewById(R.id.plotsSpringButton);
+        menuSpringButton=(Button)findViewById(R.id.menuSpringButton);
+
+        Intent gi=getIntent();
+        xList=gi.getDoubleArrayExtra("xList");
+        vList=gi.getDoubleArrayExtra("vList");
+        aList=gi.getDoubleArrayExtra("aList");
+        m=gi.getDoubleExtra("m",0);
+        g=gi.getDoubleExtra("g",0);
+        k=gi.getDoubleExtra("k",0);
+        mgkView.setText("m="+m+" kg     "+"g="+g+" m/(sec^2)\nk="+k+" N/m");
+
+        String[] list=new String[xList.length+1];
+        list[0]="t(sec)    x(m)    v(m/sec)  a(m/sec^2)";
+
+        for(int i=1; i<xList.length+1; i++)
+        {
+            String t=" ";
+            String time=String.valueOf((double)(i-1)/100);
+            int dot=time.indexOf('.');
+            if(time.length()>dot+digitsAfterDot+1)
+            {
+                time=time.substring(0,dot+digitsAfterDot+1);
+            }
+            else
+            {
+                while(time.length()<=dot+digitsAfterDot+1)
+                {
+                    time+=" ";
+                }
+                if(i%10==0)time+="  ";
+            }
+            t+=time+"      ";
+
+            String x=String.valueOf(xList[i-1]);
+            dot=x.indexOf('.');
+            if(x.length()>dot+digitsAfterDot+1)
+            {
+                x=x.substring(0,dot+digitsAfterDot+1);
+            }
+            else
+            {
+                while(x.length()<=dot+digitsAfterDot+1)
+                {
+                    x+=" ";
+                }
+            }
+            t+=x+"      ";
+
+            String v=String.valueOf(vList[i-1]);
+            dot=v.indexOf('.');
+            if(v.length()>dot+digitsAfterDot+1)
+            {
+                v=v.substring(0,dot+digitsAfterDot+1);
+            }
+            else
+            {
+                while(v.length()<=dot+digitsAfterDot+1)
+                {
+                    v+=" ";
+                }
+            }
+            t+=v+"        ";
+
+            String a=String.valueOf(aList[i-1]);
+            dot=a.indexOf('.');
+            if(a.length()>dot+digitsAfterDot+1)
+            {
+                a=a.substring(0,dot+digitsAfterDot+1);
+            }
+            else
+            {
+                while(a.length()<=dot+digitsAfterDot+1)
+                {
+                    a+=" ";
+                }
+            }
+            Log.w("a=",a);
+            t+=a;
+            Log.w("t=",t);
+
+            list[i]=t;
+        }
+
+        ArrayAdapter<String> adp=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,list);
+        resultsSpringLV.setAdapter(adp);
     }
 }
