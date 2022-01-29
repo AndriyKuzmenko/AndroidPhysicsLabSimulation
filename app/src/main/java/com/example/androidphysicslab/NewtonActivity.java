66a7533be0336bb16e4ef97856680b13e272383e
Andriy <ak8022@bs.amalnet.k12.il>
2022-01-29 18:49:33 +0200

    Finished animation

--- app/src/main/java/com/example/androidphysicslab/NewtonActivity.java
+++ app/src/main/java/com/example/androidphysicslab/NewtonActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +24,7 @@ public class NewtonActivity extends AppCompatActivity
     NewtonView newtonView;
     int planet;
     double m1,m2,g,mu,pixelsPerMeter,a,maxLength;
+    final int seconds=5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -49,28 +51,31 @@ public class NewtonActivity extends AppCompatActivity
         }
         else
         {
-            maxLength=0.5*a*100;
-            maxLength*=2.2;
+            maxLength=0.5*a*seconds*seconds;
+            maxLength*=0.005;
         }
 
         pixelsPerMeter=pixelsPerMeter=(double) Resources.getSystem().getDisplayMetrics().heightPixels/(3*maxLength);
 
+        Log.d("a=",""+a);
+        Log.d("pixles",""+pixelsPerMeter);
+
         super.onCreate(savedInstanceState);
-        newtonView=new NewtonView(this,m1,m2,mu,g,a,pixelsPerMeter);
+        newtonView=new NewtonView(this,m1,m2,mu,g,a,pixelsPerMeter,maxLength,seconds);
         setContentView(newtonView);
     }
 }
 
 class NewtonView extends SurfaceView
 {
-    double m1,m2,mu,g,a,pixelsPerMeter;
-    int top,right,bottom;
+    double m1,m2,mu,g,a,pixelsPerMeter,x,y,maxLength,v,xMeter;
+    int top,right,bottom,counter,seconds;
     private SurfaceHolder surfaceHolder;
     private Paint paint;
     Canvas canvas;
     boolean started;
 
-    public NewtonView(Context context,double m1,double m2,double mu,double g,double a,double pixelsPerMeter)
+    public NewtonView(Context context,double m1,double m2,double mu,double g,double a,double pixelsPerMeter,double maxLength,int seconds)
     {
         super(context);
 
@@ -80,6 +85,11 @@ class NewtonView extends SurfaceView
         this.g=g;
         this.a=a;
         this.pixelsPerMeter=pixelsPerMeter;
+        this.maxLength=maxLength;
+        this.seconds=seconds;
+        v=0;
+        xMeter=0;
+        counter=0;
 
         paint=new Paint();
         surfaceHolder=getHolder();
@@ -88,6 +98,8 @@ class NewtonView extends SurfaceView
         top=3*Resources.getSystem().getDisplayMetrics().heightPixels/5;
         right=3*Resources.getSystem().getDisplayMetrics().widthPixels/4;
         bottom=Resources.getSystem().getDisplayMetrics().heightPixels;
+        y=top+50;
+        x=right-maxLength*pixelsPerMeter+25;
     }
 
     @Override
@@ -107,16 +119,38 @@ class NewtonView extends SurfaceView
                     {
                         canvas = surfaceHolder.lockCanvas();
                         drawDesk(canvas);
+                        drawBoxes(canvas);
                         surfaceHolder.unlockCanvasAndPost(canvas);
+                        v+=a*0.01;
+                        xMeter+=v*0.01;
+                        x+=v*pixelsPerMeter*0.01;
+                        y+=v*pixelsPerMeter*0.01;
+
+                        counter++;
+                        if(counter==25)
+                        {
+                            t.cancel();
+                        }
                     }
 
                     public void drawDesk(Canvas canvas)
                     {
                         canvas.drawColor(Color.YELLOW);
-                        paint.setColor(Color.RED);
+                        paint.setColor(Color.BLUE);
                         canvas.drawRect(new Rect(0,top,right,bottom),paint);
                         paint.setColor(Color.rgb(55,0,55));
                         canvas.drawCircle(right+30,top-30,42,paint);
+                        paint.setColor(Color.rgb(0,0,55));
+                        paint.setStrokeWidth(5);
+                        canvas.drawLine((int)x-2,top-72,right+23,top-72,paint);
+                        canvas.drawLine(right+72,top-30,right+72,(int)y+5,paint);
+                    }
+
+                    public void drawBoxes(Canvas canvas)
+                    {
+                        paint.setColor(Color.RED);
+                        canvas.drawRect(new Rect((int)x-178,top-144,(int)x,top),paint);
+                        canvas.drawRect(new Rect(right+5,(int)y,right+139,(int)y+100),paint);
                     }
                 },5,5);
             }
