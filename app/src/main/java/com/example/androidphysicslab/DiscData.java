8f5155520265066153f591b5607a9c2108adfa1b
Andriy <ak8022@bs.amalnet.k12.il>
2022-02-10 19:39:19 +0200

    Disc data

--- app/src/main/java/com/example/androidphysicslab/DiscData.java
+++ app/src/main/java/com/example/androidphysicslab/DiscData.java
@@ -1,21 +1,30 @@
 package com.example.androidphysicslab;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DiscData extends AppCompatActivity
+public class DiscData extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     TextView massDiscLabel,muDiscLabel,kDiscLabel,shiftDiscLabel;
     EditText massDiscET,muDiscET,kDiscET,shiftDiscET;
     Spinner planetDiscSpinner;
     Button startButton;
+    AlertDialog.Builder adb;
+    int planet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,6 +42,7 @@ public class DiscData extends AppCompatActivity
         startButton=(Button)findViewById(R.id.startButton);
         shiftDiscLabel=(TextView)findViewById(R.id.shiftDiscLabel);
         shiftDiscET=(EditText)findViewById(R.id.shiftDiscET);
+        planet=0;
 
         changeLanguage();
     }
@@ -70,5 +80,98 @@ public class DiscData extends AppCompatActivity
         kDiscLabel.setText(Languages.elasticConstant);
         startButton.setText(Languages.start);
         shiftDiscLabel.setText(Languages.shiftDistance);
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item,Languages.planets);
+        planetDiscSpinner.setAdapter(adp);
+    }
+
+    public void start()
+    {
+        if(planet==2)
+        {
+            adb=new AlertDialog.Builder(this);
+            adb.setMessage(Languages.whatIsTheGravityOfEarth);
+
+            adb.setPositiveButton("9.807", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    startAnimation(false);
+                }
+            });
+
+            adb.setNegativeButton("10", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    startAnimation(true);
+                }
+            });
+
+            AlertDialog ad=adb.create();
+            ad.show();
+        }
+        else
+        {
+            startAnimation(false);
+        }
+    }
+
+    public void startAnimation(boolean b)
+    {
+        Intent si=new Intent(this,NewtonActivity.class);
+
+        Log.i("TAG","planet="+planet+", g="+Languages.gravity[planet]);
+
+        double m1=Double.parseDouble(massDiscET.getText().toString());
+        double mu=Double.parseDouble(muDiscET.getText().toString());
+        double k=Double.parseDouble(kDiscET.getText().toString());
+        double shift=Double.parseDouble(shiftDiscET.getText().toString());
+
+        si.putExtra("m1",m1);
+        si.putExtra("mu",mu);
+        si.putExtra("k",k);
+        si.putExtra("shift",shift);
+        if(b) si.putExtra("planet",-1);
+        else     si.putExtra("planet",planet);
+
+        startActivity(si);
+    }
+
+    /**
+     * <p>Callback method to be invoked when an item in this view has been
+     * selected. This callback is invoked only when the newly selected
+     * position is different from the previously selected position or if
+     * there was no selected item.</p>
+     * <p>
+     * Implementers can call getItemAtPosition(position) if they need to access the
+     * data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the selection happened
+     * @param view     The view within the AdapterView that was clicked
+     * @param position The position of the view in the adapter
+     * @param id       The row id of the item that is selected
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        planet=position;
+        Log.d("planet=",""+planet);
+    }
+
+    /**
+     * Callback method to be invoked when the selection disappears from this
+     * view. The selection can disappear for instance when touch is activated
+     * or when the adapter becomes empty.
+     *
+     * @param parent The AdapterView that now contains no selected item.
+     */
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
     }
 }
