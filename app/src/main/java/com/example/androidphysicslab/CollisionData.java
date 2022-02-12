b6e5b948c2adad929982e01a8b11e76258c6b5e5
Andriy <ak8022@bs.amalnet.k12.il>
2022-02-12 15:36:59 +0200

    collision data

--- app/src/main/java/com/example/androidphysicslab/CollisionData.java
+++ app/src/main/java/com/example/androidphysicslab/CollisionData.java
@@ -1,15 +1,24 @@
 package com.example.androidphysicslab;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CollisionData extends AppCompatActivity
 {
+    TextView railHeightLabel,tableHeightLabel;
+    EditText railHeightET,tableHeightET;
+    Spinner planetCollisionSpinner;
+    Button startCollisionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_collision_data);
     }
 }
